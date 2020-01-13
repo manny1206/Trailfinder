43ec244e68154a67de36b3b6efd14cb611c54395
BuildTools <unconfigured@null.spigotmc.org>
2020-01-13 11:34:46 -0500

    signup

---
+++ src/app/user.ts
@@ -0,0 +1,12 @@
+export class user {
+    uname: string;
+    pword: string;
+    id: number;
+
+    name: string;
+    bio: string;
+    friends: number[];
+    email: string;
+    country: string;
+    status: string;
+}
